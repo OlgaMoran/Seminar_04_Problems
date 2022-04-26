807584bd5caee7d17f6192a3191711b45c51e862
Olga Moran <olga@azimuth.co.za>
2022-04-26 13:30:30 +0800

    Added Problem 1

---
+++ Problem_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// 1. Задать массив из 8 элементов и вывести их на экран
+
+void FillArray(int[] Array)
+{
+    int length = Array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Array[index] = new Random().Next(1, 21);
+        index++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(array[position]);
+        position++;
+    }
+}
+
+int[] myArray = new int[8];
+FillArray(myArray);
+PrintArray(myArray);
