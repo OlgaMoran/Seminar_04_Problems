f9348475124188b7027351b1664ac76c5051013e
Olga Moran <olga@azimuth.co.za>
2022-04-26 15:30:09 +0800

    Added Problem 5.1

---
+++ Problem_5.1/Program.cs
@@ -0,0 +1,44 @@
+﻿// 5. Определить, присутствует ли в заданном массиве некоторое число.
+
+// Уточнение от "заказчика": если нет элемента - показать «-1».
+
+//int[] myArray = { 159, 38, 91, 6, 27, 114, 74, 6, 399 };
+//int length = myArray.Length;
+
+// string searchNum(int[] Array, int find)
+// {
+// int Length = Array.Length;
+// int index = 0;
+// while (index < Length)
+// {
+//     if (Array[index] == find)
+//     {
+//         int result = 1;
+//         break;
+//     }
+//     index++;
+// }
+// return result; 
+// }
+
+// string res = searchNum(myArray, 6);
+
+int[] array = { 10, 34, 9, 634, 28, 93, 74, 9, 399 };
+int n = array.Length;
+int find = 9;
+int index = 0;
+int count = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        count++;
+        index++;
+    }
+}
+if (count == 0)
+{
+    Console.WriteLine("No!");
+}
+else Console.WriteLine("YES!");
