c1b0f0692e8e1b5e32387d175d5929a222dead35
Olga Moran <olga@azimuth.co.za>
2022-04-26 13:32:36 +0800

    Added Problem 2

---
+++ Problem_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// 2. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран.
+
+void FillArray(int[] Array)
+{
+    int length = Array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Array[index] = new Random().Next(0, 2);
+        index++;
+    }
+}
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(array[position]);
+        position++;
+    }
+}
+
+int[] myArray = new int[8];
+FillArray(myArray);
+PrintArray(myArray);
